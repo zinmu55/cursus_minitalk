a1c6a95931ae2ef1cca5c20a174906d75a6f5fd6
Shintaro Kohtake <skohtake@c1r5s8.42tokyo.jp>
2025-07-05 19:02:39 +0900

    handle 2 signals

--- signalhandle.c
+++ signalhandle.c
@@ -1,57 +1,55 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   sa_test.c                                          :+:      :+:    :+:   */
+/*   signalhandle.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: skohtake <skohtake@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/05 13:49:20 by skohtake          #+#    #+#             */
-/*   Updated: 2025/07/05 14:32:01 by skohtake         ###   ########.fr       */
+/*   Updated: 2025/07/05 19:02:17 by skohtake         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <signal.h> // sigaction, SIGUSR1用
-#include <stdio.h>  // printf用
-#include <unistd.h> // getpid, sleep用
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
 
-// 信号ハンドラ関数
-// この関数は、SIGUSR1シグナルがプロセスに送信されたときに呼び出されます。
-void	handle_sigusr1(int signum)
+void handle_signals(int signum)
 {
-	printf("信号 %d (SIGUSR1) を受信しました！\n", signum);
-	printf("これでシグナルハンドラが実行されました。\n");
+	if (signum == SIGUSR1)
+	{
+		printf("receiving the signal of --- %d (SIGUSR1) ---\n",signum);
+	}
+	else if (signum == SIGUSR2)
+	{
+		printf("receiving the signal of --- %d (SIGUSR2) ---\n",signum);
+	}
+	else
+	{
+		printf("receiving an unknown signal, whose signum is --- %d ---\n",signum);
+	}
 }
 
-int	main(void)
+int main(void)
 {
-	struct sigaction sa; // sigaction構造体を宣言
-	// 1. シグナルハンドラを設定
-	// sigaction構造体のsa_handlerに、呼び出したい関数のアドレスを設定します。
-	// この形式のハンドラは、受け取ったシグナルの番号だけを引数にとります。
-	sa.sa_handler = handle_sigusr1;
-	// 2. sa_flagsを設定 (ここでは特に設定なし、デフォルト動作)
-	// 通常はSA_RESTARTなどを設定しますが、ここでは最も単純な形にするため0（または設定なし）にします。
+	struct sigaction sa;
+	sa.sa_handler = handle_signals;
 	sa.sa_flags = 0;
-	// 3. sa_maskを設定 (ここでは何もブロックしない)
-	// sigemptysetは、シグナルハンドラ実行中にブロックするシグナルセットを空にします。
-	// つまり、このハンドラが実行中に他のどんなシグナルもブロックされません。
 	sigemptyset(&sa.sa_mask);
-	// 4. sigaction() を呼び出してハンドラを登録
-	// SIGUSR1 シグナルに対して、上で設定したsa構造体（ハンドラ情報）を登録します。
-	// 3つ目の引数（NULL）は、以前のシグナル設定を取得しないことを意味します。
-	if (sigaction(SIGUSR1, &sa, NULL) == -1)
+	if(sigaction(SIGUSR1, &sa, NULL) == -1)
 	{
-		perror("sigactionの登録に失敗しました");
-		return (1); // エラー終了
+		perror("SIGUSR1 failed\n");
+		return(1);
 	}
-	printf("このプログラムのPIDは: %d です。\n", getpid());
-	printf("別のターミナルから 'kill -SIGUSR1 %d' を実行して、SIGUSR1を送信してください。\n", getpid());
-	printf("SIGUSR1を受信するまで待機中...\n");
-	// プロセスを終了させずに、シグナルを待ち続けるためのループ
-	// ここでプログラムが一時停止し、シグナルが来るのを待ちます。
+	if(sigaction(SIGUSR2, &sa,NULL)==-1)
+	{
+		perror("SIGUSR2 failed\n");
+		return(1);
+	}
+	printf("PID of this process is --- %d ---.\n", getpid());
+	printf("Send signal...\n");
+	printf("Just waiting...\n");
 	while (1)
-	{
-		sleep(1); // CPU使用率を抑えるために1秒ごとに待機
-	}
+		;
 	return (0);
 }
