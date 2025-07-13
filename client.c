9f5bbbade4117db16e06aea406e0f58598004c7c
Shintaro Kohtake <skohtake@c1r3s7.42tokyo.jp>
2025-07-13 15:49:28 +0900

    follow norm

--- client.c
+++ client.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   client.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: shintarokohtake <shintarokohtake@studen    +#+  +:+       +#+        */
+/*   By: skohtake <skohtake@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/05 13:49:15 by skohtake          #+#    #+#             */
-/*   Updated: 2025/07/11 08:14:47 by shintarokoh      ###   ########.fr       */
+/*   Updated: 2025/07/13 15:29:59 by skohtake         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,37 +15,43 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-void client_error_msg(void)
+void	client_error_msg(void)
 {
 	ft_putstr_fd(" Error : Check how to use \"client\" program. ", 1);
 	ft_putchar_fd('\n', 1);
 }
 
+int	send_char(pid_t pid, char c)
+{
+	int	num_bit;
+	int	kill_res;
+
+	num_bit = 8;
+	while (num_bit--)
+	{
+		if (c & (1 << num_bit))
+			kill_res = kill(pid, SIGUSR2);
+		else
+			kill_res = kill(pid, SIGUSR1);
+		if (kill_res != 0)
+			return (client_error_msg(), EXIT_FAILURE);
+		usleep(100);
+	}
+	return (0);
+}
+
 int	main(int argc, char **argv)
 {
 	int		i;
-	int 	kill_res;
 	char	c;
-	int		num_bit;
 
-	if(argc != 3)
-		return (client_error_msg(),EXIT_FAILURE);
+	if (argc != 3)
+		return (client_error_msg(), EXIT_FAILURE);
 	i = 0;
 	c = (char)argv[2][i++];
-	num_bit = 8;
 	while (c)
 	{
-		while (num_bit--)
-		{
-			if (c & (1 << num_bit))
-				kill_res = kill(ft_atoi(argv[1]), SIGUSR2);
-			else
-				kill_res = kill(ft_atoi(argv[1]), SIGUSR1);
-			if(kill_res != 0)
-				return(client_error_msg(),EXIT_FAILURE);
-			usleep(100);
-		}
-		num_bit = 8;
+		send_char(ft_atoi(argv[1]), c);
 		c = (char)argv[2][i++];
 	}
 	return (EXIT_SUCCESS);
